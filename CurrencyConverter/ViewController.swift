5cd4894403ba2736082ae64fd9b7482c5378a0e2
Ivan Kulundžić <ivan.kulundzic.mail@gmail.com>
2019-10-26 09:02:56 +0200

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Ivan Kulundzic on 26/10/2019.
+//  Copyright © 2019 Ivan Kulundzic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
